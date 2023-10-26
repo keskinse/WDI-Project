9b0dc8e8f2237369f08df4db82e52c4144bc81ad
Ada Verdha <101117062+adaverdha@users.noreply.github.com>
2023-10-26 11:19:01 +0200

    Add files via upload

--- src/model/DirectorXMLReader.java
+++ src/model/DirectorXMLReader.java
@@ -1,49 +1,29 @@
 package model;
 
-import java.util.List;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+import java.time.temporal.ChronoField;
+import java.util.Locale;
 
 import org.w3c.dom.Node;
 
-import de.uni_mannheim.informatik.dws.winter.model.DataSet;
 import de.uni_mannheim.informatik.dws.winter.model.defaultmodel.Attribute;
 import de.uni_mannheim.informatik.dws.winter.model.io.XMLMatchableReader;
 
-public class DBpediaXMLReader extends XMLMatchableReader<DBpedia, Attribute> {
+public class DirectorXMLReader extends XMLMatchableReader<Director, Attribute> {
 
 	@Override
-	protected void initialiseDataset(DataSet<DBpedia, Attribute> dataset) {
-		super.initialiseDataset(dataset);
+	public Director createModelFromElement(Node node, String provenanceInfo) {
+		String id = getValueFromChildElement(node, "id");
 
-	}
+		// create the object with id and provenance information
+		Director director = new Director(id);
 
-	@Override
-	public DBpedia createModelFromElement(Node node, String provenanceInfo) {
-		String id = getValueFromChildElement(node, "movie_id");
+		// fill the attributes
+		director.setDirector(getValueFromChildElement(node, "director"));
 
-// create the object with id and provenance information
-		DBpedia movie = new DBpedia(id, provenanceInfo);
-
-// fill the attributes
-		movie.setTitle(getValueFromChildElement(node, "title"));
-
-		// movie.setDirectors(getValueFromChildElement(node, "directors"));
-
-
-// load the list of actors, directors, producers
-		
-		List<Director> directors = getObjectListFromChildElement(node, "directors", "director", new DirectorXMLReader(),
-				provenanceInfo);
-		movie.setDirectors(directors);
-		
-		List<Actor> actors = getObjectListFromChildElement(node, "actors", "actor", new ActorXMLReader(),
-				provenanceInfo);
-		movie.setActors(actors);
-		
-		List<Producer> producers = getObjectListFromChildElement(node, "producers", "producer", new ProducerXMLReader(),
-				provenanceInfo);
-		movie.setProducers(producers);
-
-		return movie;
+		return director;
 	}
 
 }
